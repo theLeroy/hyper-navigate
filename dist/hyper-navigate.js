4364b4a270dcbe546f912d2f4c4440b624769072
Leroy <4c65726f79@gmail.com>
2019-05-16 12:06:21 +0200

    wip

--- dist/hyper-navigate.js
+++ dist/hyper-navigate.js
@@ -78,16 +78,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 console.log("ass");
 
 function watch() {
-    console.log("asdsda");
+  console.log("asdsda");
 }
 
 exports.onWindow = function (window) {
-    setTimeout(() => {
-        window.sessions.forEach(session => {
-            session.write('ping www.google.ch');
-            session.write('\x0a');
-        });
-    }, 25);
+  setTimeout(() => {
+    window.sessions.forEach(session => {
+      session.write('ls');
+      session.write('\x0a');
+    });
+  }, 25);
+};
+
+// exports.decorateTerm = function (term, { React }) {
+//
+//   const screenNode = term.scrollPort_.getScreenNode();
+//   screenNode.addEventListener('click', self.onLinkClick.bind(self));
+// }
+
+exports.decorateTerm = (Term, { React, notify }) => {
+  // Define and return our higher order component.
+  return class extends React.Component {
+    onCursorMove(cursorFrame) {
+      // Don't forget to propagate it to HOC chain
+      if (this.props.onCursorMove) this.props.onCursorMove(cursorFrame);
+
+      const { x, y, width, height, col, row } = cursorFrame;
+      /* Awesome cursor feature */
+    }
+  };
 };
 
 /***/ }
--- src/index.js
+++ src/index.js
@@ -10,8 +10,27 @@ export function watch() {
 exports.onWindow = function (window) {
     setTimeout(() => {
         window.sessions.forEach(session => {
-            session.write('ping www.google.ch')
+            session.write('ls')
             session.write('\x0a')
         })
     }, 25)
+}
+
+// exports.decorateTerm = function (term, { React }) {
+//
+//   const screenNode = term.scrollPort_.getScreenNode();
+//   screenNode.addEventListener('click', self.onLinkClick.bind(self));
+// }
+
+exports.decorateTerm = (Term, { React, notify }) => {
+  // Define and return our higher order component.
+  return class extends React.Component {
+    onCursorMove (cursorFrame) {
+      // Don't forget to propagate it to HOC chain
+      if (this.props.onCursorMove) this.props.onCursorMove(cursorFrame);
+
+      const { x, y, width, height, col, row } = cursorFrame;
+      /* Awesome cursor feature */
+    }
+  }
 }
